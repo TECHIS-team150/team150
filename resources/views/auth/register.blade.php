49d8a462136b18d940c80271d1d3a5bcac603e5d
dk747 <120307299+dk747@users.noreply.github.com>
2023-05-12 15:51:33 +0900

    チェックボックス削除

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,7 +37,7 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-                        <div class="form-group row">
+                        <!-- <div class="form-group row">
                             <label for="role" class="col-md-4 col-form-label text-md-end">{{ __('管理者権限') }}</label>
                             <div class="col-md-6">
                                 <div class="form-check">
@@ -45,7 +45,7 @@
                                     <label class="form-check-label" for="role">{{ __('管理者') }}</label>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
